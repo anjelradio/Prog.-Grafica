7ef677cfbeaceed27f2b5c4b24a1df78b97d8fc6
anjelradio <anjelradio@gmail.com>
2025-04-15 17:58:13 -0400

    Reestructuracion

---
+++ U 3d/U 3d/Clases/Escenario.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using OpenTK.Mathematics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace U_3d.Clases
+{
+    public class Escenario
+    {
+        private List<Objeto> _objetos;
+        private Camara _camara;
+        private Shader _shader;
+        private Matrix4 _projection;
+        private float _rotacionTotal = 0.0f;
+        private bool _pausado = false;
+        private float _separacionEntreUs = 2.5f;
+        private bool _inicializado = false;
+
+        public Escenario(int cantidadU)
+        {
+            _camara = new Camara();
+            _shader = new Shader();
+            _objetos = new List<Objeto>();
+
+            // Crear objetos U según la cantidad especificada
+            for (int i = 0; i < cantidadU; i++)
+            {
+                float posicionX = i * _separacionEntreUs;
+                Objeto objeto = new Objeto(
+                    new Puntos(posicionX, 0, 0),
+                    2.0f, 2.0f, 1.0f,
+                    new Vector3(1.0f, 1.0f, 1.0f)
+                );
+                _objetos.Add(objeto);
+            }
+
+            // Posicionar la cámara frente al primer objeto
+            if (_objetos.Count > 0)
+            {
+                Vector3 posicionPrimerObjeto = _objetos[0].ObtenerPosicion();
+                _camara.ActualizarPosicion(new Vector3(posicionPrimerObjeto.X, posicionPrimerObjeto.Y, posicionPrimerObjeto.Z + 10.0f));
+            }
+        }
+
+        public void Inicializar(float width, float height)
+        {
+            if (_inicializado)
+                return;
+
+            GL.Enable(EnableCap.DepthTest);
+
+            _shader.Use();
+
+            // Inicializar todos los objetos
+            foreach (var objeto in _objetos)
+            {
+                objeto.Inicializar(_shader);
+            }
+
+            ActualizarProyeccion(width, height);
+            _inicializado = true;
+        }
+
+        public void ActualizarProyeccion(float width, float height)
+        {
+            _projection = Matrix4.CreatePerspectiveFieldOfView(
+                MathHelper.DegreesToRadians(45.0f),
+                width / height,
+                0.1f,
+                100.0f
+            );
+
+            _shader.Use();
+            _shader.SetMatrix4("projection", _projection);
+        }
+
+        public void Actualizar(KeyboardState input, float deltaTime)
+        {
+            // Procesar pausado de rotación
+            if (input.IsKeyPressed(Keys.Space))
+                _pausado = !_pausado;
+
+            // Actualizar cámara
+            _camara.ProcesarMovimiento(input, deltaTime);
+
+            // Actualizar rotación global si no está pausado
+            if (!_pausado)
+                _rotacionTotal += 15.0f * deltaTime;
+
+            // Actualizar rotación de cada objeto
+            foreach (var objeto in _objetos)
+            {
+                objeto.SetRotacionY(_rotacionTotal);
+            }
+        }
+
+        public void Renderizar()
+        {
+            _shader.Use();
+
+            // Configurar matriz de vista
+            var view = _camara.ObtenerMatrizVista();
+            _shader.SetMatrix4("view", view);
+
+            // Dibujar ejes de coordenadas
+            DibujarEjes();
+
+            // Renderizar cada objeto
+            foreach (var objeto in _objetos)
+            {
+                objeto.Dibujar(_shader);
+            }
+        }
+
+        private void DibujarEjes()
+        {
+            // Dibujar ejes X, Y, Z para referencia
+            GL.LineWidth(3.0f);
+            GL.Begin(PrimitiveType.Lines);
+
+            // eje X (rojo)
+            GL.Color3(1.0f, 0.0f, 0.0f);
+            GL.Vertex3(-5.0f, 0.0f, 0.0f);
+            GL.Vertex3(5.0f, 0.0f, 0.0f);
+
+            // eje Y (verde)
+            GL.Color3(0.0f, 1.0f, 0.0f);
+            GL.Vertex3(0.0f, -5.0f, 0.0f);
+            GL.Vertex3(0.0f, 5.0f, 0.0f);
+
+            // eje Z (azul)
+            GL.Color3(0.0f, 0.0f, 1.0f);
+            GL.Vertex3(0.0f, 0.0f, -5.0f);
+            GL.Vertex3(0.0f, 0.0f, 5.0f);
+
+            GL.End();
+        }
+
+        public void Liberar()
+        {
+            foreach (var objeto in _objetos)
+            {
+                objeto.Liberar();
+            }
+
+            GL.UseProgram(0);
+            GL.DeleteProgram(_shader.Handle);
+        }
+    }
+}
---
+++ U 3d/U 3d/Clases/Figura.cs
@@ -0,0 +1,117 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using System.Collections.Generic;
+
+namespace U_3d.Clases
+{
+    public class Figura
+    {
+        // struct para vértices
+        private struct Vertice
+        {
+            public Vector3 Posicion;
+            public Vector3 Color;
+
+            public Vertice(Vector3 posicion, Vector3 color)
+            {
+                Posicion = posicion;
+                Color = color;
+            }
+        }
+
+        private List<Vertice> _vertices;
+        private List<uint> _indices;
+        private Vector3 _origen;
+        private float _ancho;
+        private float _alto;
+        private float _profundidad;
+        private Vector3 _color;
+
+        public Figura(Puntos origen, float ancho, float alto, float profundidad, Vector3 color)
+        {
+            _origen = origen.ToVector3();
+            _ancho = ancho;
+            _alto = alto;
+            _profundidad = profundidad;
+            _color = color;
+
+            InicializarGeometria();
+        }
+
+        private void InicializarGeometria()
+        {
+            _vertices = new List<Vertice>();
+            _indices = new List<uint>();
+
+            //crear vertices para la forma U
+            CrearVertices();
+            //definir índices para las líneas
+            CrearIndices();
+        }
+
+        private void CrearVertices()
+        {
+            //calcular posiciones 
+            float x = _origen.X;
+            float y = _origen.Y;
+            float z = _origen.Z;
+
+            //cara frontal
+            _vertices.Add(new Vertice(new Vector3(x, y, z + _profundidad / 2), _color));                        // 0
+            _vertices.Add(new Vertice(new Vector3(x, y + _alto, z + _profundidad / 2), _color));                // 1
+            _vertices.Add(new Vertice(new Vector3(x + _ancho * 0.2f, y + _alto, z + _profundidad / 2), _color));  // 2
+            _vertices.Add(new Vertice(new Vector3(x + _ancho * 0.2f, y + _alto * 0.2f, z + _profundidad / 2), _color)); // 3
+            _vertices.Add(new Vertice(new Vector3(x + _ancho * 0.8f, y + _alto * 0.2f, z + _profundidad / 2), _color)); // 4
+            _vertices.Add(new Vertice(new Vector3(x + _ancho * 0.8f, y + _alto, z + _profundidad / 2), _color));  // 5
+            _vertices.Add(new Vertice(new Vector3(x + _ancho, y + _alto, z + _profundidad / 2), _color));       // 6
+            _vertices.Add(new Vertice(new Vector3(x + _ancho, y, z + _profundidad / 2), _color));               // 7
+
+            //cara trasera
+            _vertices.Add(new Vertice(new Vector3(x, y, z - _profundidad / 2), _color));                        // 8
+            _vertices.Add(new Vertice(new Vector3(x, y + _alto, z - _profundidad / 2), _color));                // 9
+            _vertices.Add(new Vertice(new Vector3(x + _ancho * 0.2f, y + _alto, z - _profundidad / 2), _color));  // 10
+            _vertices.Add(new Vertice(new Vector3(x + _ancho * 0.2f, y + _alto * 0.2f, z - _profundidad / 2), _color)); // 11
+            _vertices.Add(new Vertice(new Vector3(x + _ancho * 0.8f, y + _alto * 0.2f, z - _profundidad / 2), _color)); // 12
+            _vertices.Add(new Vertice(new Vector3(x + _ancho * 0.8f, y + _alto, z - _profundidad / 2), _color));  // 13
+            _vertices.Add(new Vertice(new Vector3(x + _ancho, y + _alto, z - _profundidad / 2), _color));       // 14
+            _vertices.Add(new Vertice(new Vector3(x + _ancho, y, z - _profundidad / 2), _color));               // 15
+        }
+
+        private void CrearIndices()
+        {
+            //parte Frontal de la U
+            _indices.AddRange(new uint[] { 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 0 });
+
+            // parte Trasera de la U
+            _indices.AddRange(new uint[] { 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 8 });
+
+            // conexiones entre la parte frontal y trasera
+            _indices.AddRange(new uint[] { 0, 8, 1, 9, 2, 10, 3, 11, 4, 12, 5, 13, 6, 14, 7, 15 });
+        }
+
+        public (float[] VertexData, uint[] Indices) ObtenerDatosRenderizado()
+        {
+            // convertir lista de vértices a formato para el buffer
+            List<float> vertexData = new List<float>();
+            foreach (var vertice in _vertices)
+            {
+                vertexData.Add(vertice.Posicion.X);
+                vertexData.Add(vertice.Posicion.Y);
+                vertexData.Add(vertice.Posicion.Z);
+                vertexData.Add(vertice.Color.X);
+                vertexData.Add(vertice.Color.Y);
+                vertexData.Add(vertice.Color.Z);
+            }
+
+            return (vertexData.ToArray(), _indices.ToArray());
+        }
+
+        // Configuración para dibujar las líneas
+        public void DibujarLineas(int indicesLength)
+        {
+            GL.LineWidth(2.0f);
+            GL.Enable(EnableCap.LineSmooth);
+            GL.DrawElements(PrimitiveType.Lines, indicesLength, DrawElementsType.UnsignedInt, 0);
+        }
+    }
+}
